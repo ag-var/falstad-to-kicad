6a5a937e8564c7501c2fe37960dedffa1c65ace1
ag-var <agastyav@proton.me>
2026-06-30 21:37:15 -0500

    added initial file opening function

---
+++ parser.py
@@ -0,0 +1,23 @@
+import sys
+import os
+
+def read_file():
+
+    if len(sys.argv) < 2:
+        print("incorrect usage. usage: python circuit_parse.py <filename>")
+        sys.exit(1)
+
+    file_name = sys.argv[1]
+
+    if not (os.path.exists(file_name)):
+        print(f"Error: the file '{file_name}' does not exist")
+        sys.exit(1)
+
+    try:
+        with open(file_name, 'r') as file:
+            print(file.read())
+    except:
+        pass
+
+if __name__ == "__main__":
+    read_file()
